1dada87afecbce79bc8c21e14ba93f43dd410db2
Julio C. Diaz <juliocdiazramirez@gmail.com>
2015-11-24 16:42:06 -0800

    Basic Todo List in Laravel

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!--// resources/views/layouts/app.blade.php-->
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Laravel Quickstart - Basic</title>
+
+        <!-- CSS And JavaScript -->
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <!-- Navbar Contents -->
+            </nav>
+        </div>
+
+        @yield('content')
+    </body>
+</html>
